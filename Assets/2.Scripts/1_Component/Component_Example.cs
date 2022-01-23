c5fb2c61750ea2165f4328072ed5e82b7ed02619
vihwan <vihwan@gmail.com>
2022-01-23 21:57:42 +0900

    Study Component Pattern

---
+++ Assets/2.Scripts/1_Component/Component_Example.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum MoveTo
+{
+    Move_Left,
+    Move_Right
+}
+
+public class Component_Example : MonoBehaviour
+{
+    MoveTo move = MoveTo.Move_Right;
+    void Start()
+    {
+        StartCoroutine("Move");
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            if (transform.position.x < -4)
+            {
+                move = MoveTo.Move_Right;
+            }
+            else if (transform.position.x > 4)
+            {
+                move = MoveTo.Move_Left;
+            }
+
+            if (move == MoveTo.Move_Right)
+            {
+                transform.Translate(1.0f * Vector3.right, Space.World);
+            }
+            else
+            {
+                transform.Translate(-1.0f * Vector3.right, Space.World);
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
+
+
---
+++ Assets/2.Scripts/1_Component/RotateAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAction : MonoBehaviour
+{
+    private void Start() {
+        StartCoroutine("Rotate");
+    }
+
+    IEnumerator Rotate()
+    {
+        while(true)
+        {
+            transform.Rotate(45.0f * Vector3.up);
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
